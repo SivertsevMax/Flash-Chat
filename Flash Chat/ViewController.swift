ea5e5e8ec662ee38a6878d71b6daf98fda9ac82b
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-03-22 11:31:32 +0300

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Максим Сиверцев on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
