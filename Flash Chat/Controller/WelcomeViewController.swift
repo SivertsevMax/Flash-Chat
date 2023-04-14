bcd4fcb0caecab04536332bfb8b3a455919c6f68
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-04-14 12:39:49 +0300

    1

--- Flash Chat/Controller/WelcomeViewController.swift
+++ Flash Chat/Controller/WelcomeViewController.swift
@@ -1,25 +1,17 @@
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var registerButton: UIButton!
-    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var mainTitle: CLTypingLabel!
     @IBOutlet weak var logInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         registerButton.layer.cornerRadius = 15
         logInButton.layer.cornerRadius = 15
-        mainTitle.text = ""
         
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for char in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.mainTitle.text?.append(char)
-            }
-            charIndex += 1
-        }
         
     }
     
