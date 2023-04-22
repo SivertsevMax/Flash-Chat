8389d05a31f9ce5449eb2a3596f50236a3debfd5
Sivertsev Maxim <sivertsev1995@icloud.com>
2023-04-22 22:53:10 +0300

    1

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -4,20 +4,50 @@ import FirebaseCore
 import FirebaseFirestore
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     var messeges: [Message] = [
-        Message(sender: "1", body: "q"),
-        Message(sender: "2", body: "w"),
-        Message(sender: "1", body: "e")
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?"),
+        Message(sender: "1", body: "hello"),
+        Message(sender: "2", body: "hey!"),
+        Message(sender: "1", body: "what's up?")
     ]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: K.cellIdentifier)
+        tableView.dataSource = self
         
     }
     
@@ -29,7 +59,7 @@ class ChatViewController: UIViewController {
             try Auth.auth().signOut()
             navigationController?.popToRootViewController(animated: false)
         } catch let signOutError as NSError {
-          print("Error signing out: %@", signOutError)
+            print("Error signing out: %@", signOutError)
             let alert = UIAlertController(title: "ERROR", message: signOutError.localizedDescription, preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "close", style: UIAlertAction.Style.default))
             self.present(alert, animated: false)
@@ -37,4 +67,16 @@ class ChatViewController: UIViewController {
         
     }
     
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.messeges.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel!.text = self.messeges[indexPath.row].body
+        return cell
+    }
 }
